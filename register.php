1277888441d0045b9c56e0901de20f35b3af1101
Bvorfi2002 <127106576+Bvorfi2002@users.noreply.github.com>
2023-06-05 23:11:26 +0200

    3 commit

--- register.php
+++ register.php
@@ -1,3 +1,32 @@
+<?php
+
+include 'config.php';
+
+if(isset($_POST['submit'])){
+
+  $name=mysqli_real_escape_string($conn,$_POST['name']);
+  $email=mysqli_real_escape_string($conn,$_POST['email']);
+  $password=mysqli_real_escape_string($conn,$_POST['password']);
+  $cpass=mysqli_real_escape_string($conn,$_POST['cpassword']);
+  $user_type=$_POST['user_type'];
+
+  $select_users=mysqli_query($conn,"SELECT * FROM `users` WHERE email='$email' AND password='$pass'") or die('query failed');
+
+if(mysqli_num_rows($select_users)>0){
+  $message[]='user already exist!';
+}
+else{
+  if($pass != $cpass){
+    $message[]='confrim password not matched!';
+  }
+  else{
+    mysqli_query($conn,"INSERT INTO `users`(name,email,password,user_type) VALUES('$name','$email','$cpass','user_type')") or die('query failed');
+    $message[]='registered successfully';
+  }
+}
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -12,14 +41,13 @@
     </head>
     <body>
       <div class="form-container">
-
       <form action="" method="post">
         <h3>Register now</h3>
         <input type="text" name="name" placeholder="Enter your name" required class="box">
         <input type="email" name="email" placeholder="Enter your email" required class="box">
         <input type="password" name="password" placeholder="Enter your password" required class="box">
         <input type="password" name="cpassword" placeholder="Confirm your password" required class="box">
-        <select name="user_type">
+        <select name="user_type" class="box">
             <option value="user">User</option>
             <option value="Admin">Admin</option>
         </select>
