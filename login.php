bc6e40257d40e269a4ddb263178bfc514db8c226
jlosha23 <Julilosha1@gmail.com>
2023-06-06 04:03:58 +0200

    home page

--- login.php
+++ login.php
@@ -1,30 +1,82 @@
+<?php
+
+include 'config.php';
+session_start();
+
+if(isset($_POST['submit'])){
+
+
+  $email=mysqli_real_escape_string($conn,$_POST['email']);
+  $pass=mysqli_real_escape_string($conn,$_POST['password']);
+  
+
+  $select_users=mysqli_query($conn,"SELECT * FROM `users` WHERE email='$email' AND password='$pass'") or die('query failed');
+
+if(mysqli_num_rows($select_users)>0){
+
+  $row = mysqli_fetch_assoc($select_users);
+
+  if($row['user_type'] == 'admin'){
+
+    $_SESSION['admin_name'] = $row['name'];
+    $_SESSION['admin_email'] = $row['email'];
+    $_SESSION['admin_id'] = $row['id'];
+    header('location:admin_page.php');
+  }
+
+   
+  
+  elseif($row['user_type'] == 'user'){
+
+    $_SESSION['user_name'] = $row['name'];
+    $_SESSION['user_email'] = $row['email'];
+    $_SESSION['user_id'] = $row['id'];
+    header('location:home.php');
+  }
+
+}
+else{
+  $message[]='Incorrect Email Or Password!';
+}
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport"content="width=device-width,initial-scale=1.0">
-        <title>Register</title>
+        <title>Login</title>
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-      <div class="form-container">
 
+    
+
+<?php
+if (isset($message)){
+  foreach($message as $message){
+    echo '
+    <div class="message">
+      <span>'.$message.'</span>
+      <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+    </div>
+    ';
+  }
+}
+?>
+
+      <div class="form-container">
       <form action="" method="post">
-        <h3>Register now</h3>
-        <input type="text" name="name" placeholder="Enter your name" required class="box">
+        <h3>Login now</h3>
         <input type="email" name="email" placeholder="Enter your email" required class="box">
         <input type="password" name="password" placeholder="Enter your password" required class="box">
-        <input type="password" name="cpassword" placeholder="Confirm your password" required class="box">
-        <select name="user_type">
-            <option value="user">User</option>
-            <option value="Admin">Admin</option>
-        </select>
-        <input type="submit" name="submit" value="Register now" class="btn">
-        <p>Already have an account? <a href="login.php">login now</a> </p>
+        <input type="submit" name="submit" value="Login now" class="btn">
+        <p>Don't have an account? <a href="register.php">Register Now</a> </p>
 
       </form>
       </div>
