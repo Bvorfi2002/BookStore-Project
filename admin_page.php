8a63e6868816abc17f87f25c4980eb016920212a
Bvorfi2002 <127106576+Bvorfi2002@users.noreply.github.com>
2023-06-06 19:49:17 +0200

    Finish Logout

--- admin_page.php
+++ admin_page.php
@@ -28,5 +28,18 @@ if(!isset($admin_id)){
     </head>
     <body>
         <?php include 'admin_header.php'; ?>
+
+
+
+        <!--custom admin js file link -->
+
+        <script src="js/admin_script.js"></script>
+
+
+
+
+
+
+
     </body>
 </html>
