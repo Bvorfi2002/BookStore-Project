818b27cc56236ae055dbe396c43dd3655c359700
Bvorfi2002 <127106576+Bvorfi2002@users.noreply.github.com>
2023-06-08 22:41:17 +0200

    Home page

---
+++ footer.php
@@ -0,0 +1,20 @@
+<section class="footer">
+
+<div class="box-container">
+
+<div class="box">
+    <h3>Quick links</h3>
+    <a href="home.php">home</a>
+
+
+</div>
+
+
+
+</div>
+
+
+
+
+
+</section>
